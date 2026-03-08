0ddb6bec56fa31e98e9e256ef8106dba29cab80f
tiven40020 <steven40020@gmail.com>
2026-03-08 18:55:12 -0500

    Se agregan paginas de politicas de tratamientos, politicas legales y Quienes somos

---
+++ src/app/pages/politica-privacidad/politica-privacidad.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-privacidad',
+  imports: [],
+  templateUrl: './politica-privacidad.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PoliticaPrivacidad { }
